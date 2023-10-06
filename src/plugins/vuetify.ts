ee747b4beec466b2bd8f298e05ddbd1b5fe80d42
Cabrini Alves <cabrinireis@gmail.com>
2023-10-06 13:36:11 -0300

    config custom theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,25 @@
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
+
+const myCustomTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
+}
+
 export default createVuetify({
   components,
   directives,
@@ -10,6 +27,9 @@ export default createVuetify({
     defaultSet: 'mdi'
   },
   theme: {
-    defaultTheme: 'dark'
+    defaultTheme: 'myCustomTheme',
+    themes: {
+      myCustomTheme
+    }
   }
 })
