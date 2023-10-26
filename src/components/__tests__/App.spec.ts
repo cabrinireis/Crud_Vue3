fd44070556d0e9edbcc352432ecaf3e85fd8a73c
Cabrini Alves <cabrinireis@gmail.com>
2023-10-26 17:33:36 -0300

    test. create test router in app

--- src/components/__tests__/App.spec.ts
+++ src/components/__tests__/App.spec.ts
@@ -1,12 +1,14 @@
 import { describe, test, expect, vi, beforeEach } from 'vitest'
 import { mount } from '@vue/test-utils'
 import App from '@/App.vue'
+import List from '@/views/ListPatient.vue'
+import Login from '@/views/LoginSusNet.vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { createTestingPinia } from '@pinia/testing'
-import { createPinia, setActivePinia } from 'pinia'
-import router from '@/router'
+import routes from '@/router/index'
+import { createRouter, createWebHashHistory } from 'vue-router'
 const vuetify = createVuetify({
   components,
   directives
@@ -14,15 +16,30 @@ const vuetify = createVuetify({
 global.ResizeObserver = require('resize-observer-polyfill')
 
 describe('App', () => {
-  beforeEach(() => {
-    setActivePinia(createPinia())
-  })
+  let wrapper = null as any
+  let router = null as any
 
-  test('should notification success', async () => {
-    const el = document.createElement('body')
-    document.body.appendChild(el)
+  beforeEach(async () => {
+    router = createRouter({
+      history: createWebHashHistory(import.meta.env.BASE_URL),
+      routes: [
+        {
+          path: '/list',
+          name: 'list',
+          component: List
+        },
+        {
+          path: '/',
+          name: 'login',
+          component: Login
+        }
+      ]
+    })
 
-    mount(App, {
+    router.push('/')
+    await router.isReady()
+
+    wrapper = mount(App, {
       props: {},
       global: {
         plugins: [
@@ -43,6 +60,26 @@ describe('App', () => {
         ]
       }
     })
+    await sessionStorage.clear()
+  })
+
+  test('should notification success', async () => {
+    const el = document.createElement('body')
+    document.body.appendChild(el)
     expect(document.querySelector('.v-snackbar__content')?.innerHTML).toBe('Seja bem vindo(a).')
+  })
+
+  test('routing with authentication', async () => {
+    const push = vi.spyOn(router, 'push')
+    await router.push('/list')
+    expect(push).toHaveBeenCalledTimes(1)
+    expect(push).toHaveBeenCalledWith('/list')
+    expect(wrapper.findComponent(List).exists()).toBe(true)
+  })
+
+  test('routing not authentication', async () => {
+    routes.push('/list')
+    await routes.isReady()
+    expect(wrapper.findComponent(List).exists()).toBe(false)
   })
 })
